c56882c6f9b139adc08d6fe9a537914b4552f388
dongwa <783137663@qq.com>
2020-07-18 22:58:08 +0800

    fix

--- 4.串/kmp.cpp
+++ 4.串/kmp.cpp
@@ -6,7 +6,7 @@ void getNext(char t[], int next[], int len) {
   int i = 1, j = 0;
   next[1] = 0;
   while (i < len) {
-    if (j = 0 || t[i] == t[j]) {
+    if (j == 0 || t[i] == t[j]) {
       ++i;
       ++j;
       next[i] = j;  //当t[i] = t[j]时next[i +1]= next[j+1]
@@ -15,25 +15,45 @@ void getNext(char t[], int next[], int len) {
     }
   }
 }
+// 求nextval数组
+void getNextVal(char t[], int next[], int len) {
+  int i = 1, j = 0;
+  next[1] = 0;
+  while (i < len) {
+    if (j == 0 || t[i] == t[j]) {
+      ++i;
+      ++j;
+      if (t[i] != t[j])
+        next[i] = j;  //当t[i] = t[j]时next[i +1]= next[j+1]
+      else
+        next[i] = next[j];
+    } else {
+      j = next[j];
+    }
+  }
+}
 // kmp匹配过程
-int kmp(char s[], char t[], int s_len, int t_len) {
+void kmp(char s[], char t[], int s_len, int t_len) {
   int next[t_len + 10];
-  getNext(t, next, t_len);
+  // getNext(t, next, t_len);
+  getNextVal(t, next, t_len);  // next的优化版，但是只能匹配t在s中首次出现
+  cout << endl;
   int i = 1, j = 1;
   while (i <= s_len && j <= t_len) {
     if (j == 0 || s[i] == t[j]) {  // j=0时为t串重置，s[i] = t[j]为当前字母匹配
       ++i;
       ++j;
+      if (j == t_len) {
+        cout << "匹配到一个" << i - t_len << " ";
+        j = next[j];  //继续搜寻下一个
+      }
     } else {
       //出现失配
       j = next[j];
     }
   }
-  if (i > t_len)
-    return i - t_len;
-  else
-    return -1;
 }
+
 int main() {
   int m, n;
   cin >> n;
@@ -42,7 +62,6 @@ int main() {
   cin >> m;
   char s[m + 10];
   cin >> s + 1;
-  int e = kmp(s, t, m, n);
-  cout << e;
+  kmp(s, t, m, n);
   return 0;
 }
